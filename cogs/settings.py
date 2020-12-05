c2ff7cec6522964ddf1f71a6b29f7313c0830858
HackerJacker <56278210+H4ckerJ4cker@users.noreply.github.com>
2020-12-05 18:41:05 +0000

    improved autorole

--- cogs/settings.py
+++ cogs/settings.py
@@ -70,6 +70,7 @@ class Settings(Cog):
                 return
             elif role_name is None:
                 join_role = self.bot.servers[ctx.guild.id]["join_role_id"]
+                role = ctx.guild.get_role(join_role)
                 await ctx.send(f"The current autorole for new members is **{join_role.name}**")
                 return
 
