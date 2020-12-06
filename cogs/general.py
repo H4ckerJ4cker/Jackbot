18f01dc03b89d4d95476e86ef8e1463c668633f1
HackerJacker <56278210+H4ckerJ4cker@users.noreply.github.com>
2020-12-06 12:49:00 +0000

    improved delete logging

--- cogs/general.py
+++ cogs/general.py
@@ -58,7 +58,7 @@ class General(Cog):
 
     @Cog.listener()
     async def on_message_delete(self, message):
-        if self.bot.get_context(message).valid is not True:
+        if await self.bot.get_context(message).valid is not True:
             log_channel_id = self.bot.servers[message.guild.id]["logging_channel_id"]
             if log_channel_id is not None:
                 log_channel = self.bot.get_channel(log_channel_id)
