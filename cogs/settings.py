885f6578e4586cdc3a70415a1594b6e52e1ca996
Pwnker <56278210+pwnker@users.noreply.github.com>
2021-04-15 16:49:55 +0100

    Update settings.py

--- cogs/settings.py
+++ cogs/settings.py
@@ -22,7 +22,7 @@ class Settings(Cog):
             title="Settings",
         )
 
-        embed.set_footer(icon_url=self.bot.user.avatar_url, text="Type @JackBot help settings [setting name] "
+        embed.set_footer(icon_url=self.bot.user.avatar_url, text="Type @JackBot help settings [setting] "
                                                                  "for more info on a setting.")
 
         for command in self.bot.commands:
@@ -34,7 +34,7 @@ class Settings(Cog):
 
     @settings.command(aliases=['prefix', 'newprefix'])
     @commands.has_permissions(manage_guild=True)
-    async def setprefix(self, ctx, *, new_prefix=None):
+    async def prefix(self, ctx, *, new_prefix=None):
         """
         Set the prefix of the bot. Prefix cannot be longer than 5 characters.
         """
@@ -51,7 +51,7 @@ class Settings(Cog):
                 else:
                     prefix = dbprefix
 
-                await ctx.send(f"The current prefix is **{prefix}**. To change it type ``@JackBot settings setprefix "
+                await ctx.send(f"The current prefix is **{prefix}**. To change it type ``@JackBot settings prefix "
                                f"[new prefix]``")
             else:
                 await self.bot.db.execute(
@@ -66,11 +66,11 @@ class Settings(Cog):
         else:
             await ctx.send("\N{NO ENTRY SIGN} That command is only available in servers.")
 
-    @settings.command(aliases=['autorole', 'joinrole'])
+    @settings.command(aliases=['setautorole', 'joinrole'])
     @commands.has_permissions(manage_guild=True)
-    async def setautorole(self, ctx, *, role_name=None):
+    async def autorole(self, ctx, *, role_name=None):
         """
-        Set the role members will get when they join the server. To turn off autorole type ``!settings autorole reset``
+        Set the role members will get when they join the server. To turn off autorole type ``@JackBot autorole reset``
         """
         if ctx.guild is not None:
             if role_name == "reset":
@@ -86,7 +86,7 @@ class Settings(Cog):
                     self.bot.servers[ctx.guild.id] = {}
                 join_role = self.bot.servers[ctx.guild.id].get("join_role_id")
                 if join_role is None:
-                    await ctx.send(f"No autorole configured to add one type. ``@JackBot settings setautorole ["
+                    await ctx.send(f"No autorole configured to add one type. ``@JackBot settings autorole ["
                                    f"role_name]``")
                     return
                 role = ctx.guild.get_role(join_role)
@@ -114,7 +114,7 @@ class Settings(Cog):
 
     @settings.command(aliases=['polls', 'pollschannel'])
     @commands.has_permissions(manage_guild=True)
-    async def setpollchannel(self, ctx, *, poll_channel: typing.Union[TextChannel, str] = None):
+    async def polls(self, ctx, *, poll_channel: typing.Union[TextChannel, str] = None):
         """
         Set the poll channel where polls should be sent. If "reset" is passed the poll channel will be reset.
         """
@@ -126,7 +126,7 @@ class Settings(Cog):
                 )
                 self.bot.servers[ctx.guild.id]["poll_channel_id"] = None
                 await ctx.send("Poll channel disabled. If a user makes a poll it will be sent to the channel they ran "
-                               "the command in. To add a poll channel type ``@JackBot settings setpollchannel ["
+                               "the command in. To add a poll channel type ``@JackBot settings polls ["
                                "channel]`` ")
                 return
             elif poll_channel is None:
@@ -136,7 +136,7 @@ class Settings(Cog):
                 if db_channel is None:
                     await ctx.send(
                         "Poll channel is disabled. If a user makes a poll it will be sent to the channel they ran "
-                        "the command in. To add a poll channel type ``@JackBot settings setpollchannel ["
+                        "the command in. To add a poll channel type ``@JackBot settings polls ["
                         "channel]`` ")
                     return
                 else:
@@ -144,7 +144,7 @@ class Settings(Cog):
 
                     await ctx.send(
                         f"The current poll channel is is **<#{channel}>**. To change it run the command again with"
-                        " a channel as an argument. ``@JackBot settings setpollchannel [channel]``")
+                        " a channel as an argument. ``@JackBot settings polls [channel]``")
 
             elif type(poll_channel) == TextChannel:
                 await self.bot.db.execute(
@@ -165,7 +165,7 @@ class Settings(Cog):
 
     @settings.command(aliases=['logs', 'setlogs', 'setloggingchannel'])
     @commands.has_permissions(manage_guild=True)
-    async def setlogging(self, ctx, *, logs_channel: typing.Union[TextChannel, str] = None):
+    async def logging(self, ctx, *, logs_channel: typing.Union[TextChannel, str] = None):
         """
         Set the logging channel where logs should be sent. If you pass in "reset" logging channel will be reset.
         All moderation commands are logged.
@@ -178,7 +178,7 @@ class Settings(Cog):
                 )
                 self.bot.servers[ctx.guild.id]["logging_channel_id"] = None
                 await ctx.send("Logging disabled. I will not log any action. To enable run the command again with"
-                               " a channel as an argument. ``@JackBot settings setlogging [channel]``")
+                               " a channel as an argument. ``@JackBot settings logging [channel]``")
                 return
             elif logs_channel is None:
                 if ctx.guild.id not in self.bot.servers:
@@ -186,14 +186,14 @@ class Settings(Cog):
                 db_channel = self.bot.servers[ctx.guild.id].get("logging_channel_id")
                 if db_channel is None:
                     await ctx.send("Logging disabled. I will not log any action. To enable run the command again with"
-                                   " a channel as an argument. ``@JackBot settings setlogging [channel]``")
+                                   " a channel as an argument. ``@JackBot settings logging [channel]``")
                     return
                 else:
                     channel = db_channel
 
                     await ctx.send(
                         f"The current logging channel is is **<#{channel}>**. To change it run the command again with"
-                        " a channel as an argument. ``@JackBot settings setlogging [channel]``")
+                        " a channel as an argument. ``@JackBot settings logging [channel]``")
 
             elif type(logs_channel) == TextChannel:
                 await self.bot.db.execute(
@@ -213,5 +213,52 @@ class Settings(Cog):
             await ctx.send("\N{NO ENTRY SIGN} That command is only available in servers.")
 
 
+    @settings.command(aliases=['welcome', 'join', 'setwelcomechannel'])
+    @commands.has_permissions(manage_guild=True)
+    async def welcomechannel(self, ctx, *, welcome_channel: typing.Union[TextChannel, str] = None):
+        """
+        Set the welcome channel where join and leave messages will be sent. If you pass in "reset" the welcome channel will be reset.
+        """
+        if ctx.guild is not None:
+            if welcome_channel == "reset":
+                await self.bot.db.execute(
+                    "UPDATE guilds SET welcome_channel_id = null WHERE guild_id = $1;",
+                    ctx.guild.id
+                )
+                self.bot.servers[ctx.guild.id]["welcome_channel_id"] = None
+                await ctx.send("Welcome messages disabled. I will not send join or leave messages. To enable run the command again with"
+                               " a channel as an argument. ``@JackBot settings welcomechannel [channel]``")
+                return
+            elif welcome_channel is None:
+                if ctx.guild.id not in self.bot.servers:
+                    self.bot.servers[ctx.guild.id] = {}
+                db_channel = self.bot.servers[ctx.guild.id].get("welcome_channel_id")
+                if db_channel is None:
+                    await ctx.send("Welcome messages disabled. I will not send join or leave messages. To enable run the command again with a channel as an argument. ``@JackBot settings welcomechannel [channel]``")
+                    return
+                else:
+                    channel = db_channel
+
+                    await ctx.send(
+                        f"The current welcome channel is is **<#{channel}>**. To change it run the command again with"
+                        " a channel as an argument. ``@JackBot settings welcomechannel [channel]``")
+
+            elif type(welcome_channel) == TextChannel:
+                await self.bot.db.execute(
+                    "INSERT INTO guilds(guild_id, welcome_channel_id) VALUES($1, $2) ON CONFLICT (guild_id) DO UPDATE SET "
+                    "logging_channel_id = $2",
+                    ctx.guild.id,
+                    welcome_channel.id,
+                )
+                if ctx.guild.id not in self.bot.servers:
+                    self.bot.servers[ctx.guild.id] = {}
+                self.bot.servers[ctx.guild.id]["logging_channel_id"] = welcome_channel.id
+
+                await ctx.send(f"Welcome channel set to <#{welcome_channel.id}>")
+            else:
+                await ctx.send("⚠️ Channel not found, please try again.")
+        else:
+            await ctx.send("\N{NO ENTRY SIGN} That command is only available in servers.")
+            
 def setup(bot):
     bot.add_cog(Settings(bot))
