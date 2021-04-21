9f2135053e4e9b88f396f2772ff7843428694e25
Pwnker <pwnker@users.noreply.github.com>
2021-04-21 15:08:05 +0100

    fixed typo

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 <a target="_blank" href="LICENSE" title="License: MIT"><img src="https://img.shields.io/badge/License-MIT-blue.svg"></a>
 <a target="_blank" href="https://www.python.org/downloads/" title="Python version"><img src="https://img.shields.io/badge/python-%3E=_3.8.0-green.svg"></a>
 
-JackBot is a breath of fresh air for discord small discord servers who need a simple bot to help run their server smoothly without having to deal with complex web panels and configuration. If you have a small community and need to do a bit more as an admin than the built in discord functions, JackBot is for you.
+JackBot is a breath of fresh air for small discord servers who need a simple bot to help run their server smoothly without having to deal with complex web panels and configuration. If you have a small community and need to do a bit more as an admin than the built in discord functions, JackBot is for you.
 
 JackBot has various features to help you automate your server, such as autorole on join and easy to understand moderation commands and logs. JackBot also boasts various other commands such as minecraft server stats and polls, to give your server extra utility.
 
--- cogs/general.py
+++ cogs/general.py
@@ -251,9 +251,9 @@ class General(Cog):
         embed = Embed (
             title="Hi, I'm JackBot",
             description="""
-            JackBot is a breath of fresh air for discord small discord servers who need a simple bot to help run their server smoothly without having to deal with complex web panels and configuration. If you have a small community and need to do a bit more as an admin than the built in discord functions, JackBot is for you.
+            JackBot is a breath of fresh air for small discord servers who need a simple bot to help run their server smoothly without having to deal with complex web panels and configuration. If you have a small community and need to do a bit more as an admin than the built in discord functions, JackBot is for you.
 
-            JackBot has various features to help you automate your server, such as autorole on join and easy to understand moderation commands and logs. JackBot also boasts various other commands such as minecraft server stats and polls, to give your server extra utility.
+JackBot has various features to help you automate your server, such as autorole on join and easy to understand moderation commands and logs. JackBot also boasts various other commands such as minecraft server stats and polls, to give your server extra utility.
             """,
 
         )
