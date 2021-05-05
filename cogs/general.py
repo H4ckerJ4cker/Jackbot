a43d887c9237c5655f82f3deec511db45956476e
Pwnker <contact@pwnker.com>
2021-05-05 19:41:22 +0100

    fix coords msg (#25)

--- cogs/general.py
+++ cogs/general.py
@@ -121,7 +121,7 @@ class General(Cog):
                 await bot_message.add_reaction("👎")
             else:
                 await message.channel.send(f"{message.author.mention} those coordinates aren't in the correct form. "
-                                           f"Please format them `X Z <description>`",
+                                           f"Please format them `X Z <description>` or `X Y Z <description>`",
                                            delete_after=10)
 
     @Cog.listener()
